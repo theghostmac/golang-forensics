5e28b2b4a3deae343242e1959e389924022f9025
MacBobby Chibuzor <theghostmac@gmail.com>
2022-03-31 16:16:55 +0100

    image is steganographic with a zip archive

--- steganography.go
+++ steganography.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"io"
+	"log"
+	"os"
+)
+
 func main() {
-	firstFile
+	firstFile, err := os.Open("go-generated-image.jpg")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer firstFile.Close()
+
+	secondFile, err := os.Open("zipper.zip")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer secondFile.Close()
+
+	steganImage, err := os.Create("stegan-image.jpg")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer steganImage.Close()
+
+	_, err = io.Copy(steganImage, firstFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = io.Copy(steganImage, secondFile)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
