76a1cca505698f7904ef4517c7451dbf8bf3653c
MacBobby Chibuzor <theghostmac@gmail.com>
2022-03-31 14:20:38 +0100

    main.go completed

--- main.go
+++ main.go
@@ -1,1 +1,27 @@
-package golang_forensics
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+var (
+	fileInfo os.FileInfo
+	err      error
+)
+
+func main() {
+	fileInfo, err = os.Stat("target.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Name of file is: ", fileInfo.Name())
+	fmt.Println("Size of file in bytes is: ", fileInfo.Size())
+	fmt.Println("Permissions on this file is: ", fileInfo.Mode())
+	fmt.Println("Last time of modification of the file is: ", fileInfo.ModTime())
+	fmt.Println("The file is a directory: ", fileInfo.IsDir())
+	fmt.Println("The system interface type of the file is: %T\n", fileInfo.Sys())
+	fmt.Println("The system information is: : ", fileInfo.Sys())
+}
